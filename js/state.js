8fd46014c658bd1e48db972e798f3fd7e0e03ee6
Pro Bandey <195909043+Pro-Bandey@users.noreply.github.com>
2026-01-04 23:18:09 +0500

    added

--- js/state.js
+++
@@ -1,8 +0,0 @@
-export class State {
-  constructor() {
-    this.tabs = [];            // [{id, name, type, content, export}]
-    this.activeId = null;
-    this.globalQuery = '';
-    this.contextTabId = null;
-  }
-}
